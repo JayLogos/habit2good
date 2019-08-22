a69450235e2ed52bcb0f50ebe0e7630821412b7f
jhlee <jhlee@gubed.co.kr>
2019-08-22 11:50:59 +0900

    for versionCode 57
    광고 매체 변경
    ManPlus

--- app/src/main/java/kr/co/gubed/habit2good/GpointRankingBoardActivity.java
+++ app/src/main/java/kr/co/gubed/habit2good/GpointRankingBoardActivity.java
@@ -2,6 +2,7 @@ package kr.co.gubed.habit2good;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
@@ -23,6 +24,9 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.mapps.android.share.AdInfoKey;
+import com.mapps.android.view.AdInterstitialView;
+import com.mz.common.listener.AdListener;
 import com.tnkfactory.ad.TnkAdListener;
 import com.tnkfactory.ad.TnkSession;
 
@@ -68,6 +72,9 @@ public class GpointRankingBoardActivity extends AppCompatActivity implements Asy
 
     Toolbar toolbar;
     ActionBar actionbar;
+
+    private AdInterstitialView m_interView = null;
+    private Handler handler = new Handler();
 
     public GpointRankingBoardActivity() {
     }
@@ -117,7 +124,28 @@ public class GpointRankingBoardActivity extends AppCompatActivity implements Asy
     @Override
     protected void onResume() {
         super.onResume();
-        TnkSession.prepareInterstitialAd(this, TnkSession.CPC);
+        TnkSession.prepareInterstitialAd(this, TnkSession.CPC, new TnkAdListener() {
+            @Override
+            public void onClose(int i) {
+                finish();
+            }
+
+            @Override
+            public void onShow() {
+
+            }
+
+            @Override
+            public void onFailure(int i) {
+                Log.e(getClass().getName(), "TNK interstitial Ad fail: " + i);
+                finish();
+            }
+
+            @Override
+            public void onLoad() {
+
+            }
+        });
     }
 
     void init() {
@@ -248,6 +276,8 @@ public class GpointRankingBoardActivity extends AppCompatActivity implements Asy
     @Override
     public void onBackPressed() {
         //super.onBackPressed();
+
+        /*TNK
         TnkSession.showInterstitialAd(this, 1000, new TnkAdListener() {
             @Override
             public void onClose(int i) {
@@ -269,7 +299,9 @@ public class GpointRankingBoardActivity extends AppCompatActivity implements Asy
             public void onLoad() {
 
             }
-        });
+        });*/
+        /* ManPlus */
+        manPlusInterstitialView(1413, 31780, 803854);
     }
 
     @Override
@@ -426,5 +458,107 @@ public class GpointRankingBoardActivity extends AppCompatActivity implements Asy
                 }
             }
         });
+    }
+
+    private void manPlusInterstitialView(int p, int m, int s) {
+        if (m_interView == null) {
+            m_interView = new AdInterstitialView(GpointRankingBoardActivity.this);
+            m_interView.setAdListener(new AdListener() {
+
+
+                @Override
+                public void onChargeableBannerType(View view, boolean b) {
+                    handler.post(new Runnable() {
+                        public void run() {
+                            Log.i(getClass().getName(), (b) ? "no chargebale" : "chargeble");
+                        }
+                    });
+                }
+
+                @Override
+                public void onFailedToReceive(View view, int i) {
+                    final int errcode = i;
+                    handler.post(new Runnable() {
+                        public void run() {
+                            showErrorMsg(errcode);
+                        }
+
+                    });
+                }
+
+                @Override
+                public void onInterClose(View view) {
+                    finish();
+                    overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+                }
+
+                @Override
+                public void onAdClick(View view) {
+                    finish();
+                    overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+                }
+            });
+            m_interView.setViewStyle(AdInterstitialView.VIEWSTYLE.NONE);
+			/*m_interView.setUserAge("22");
+			m_interView.setUserGender("1");
+			m_interView.setLoaction(true);
+			m_interView.setAccount("test");
+			m_interView.setEmail("test@mezzomediaco.kr");*/
+        }
+        m_interView.setAdViewCode(String.valueOf(p), String.valueOf(m), String.valueOf(s));
+        m_interView.ShowInterstitialView();
+    }
+
+    private void showErrorMsg(int errorCode) {
+        String log;
+        switch (errorCode) {
+            case AdInfoKey.AD_SUCCESS:
+                log = "[ " + errorCode + " ] " + "광고 성공";
+                break;
+            case AdInfoKey.AD_ID_NO_AD:
+                log = "[ " + errorCode + " ] " + "광고 소진";
+                TnkSession.showInterstitialAd(GpointRankingBoardActivity.this);
+                break;
+            case AdInfoKey.NETWORK_ERROR:
+                log = "[ " + errorCode + " ] " + "(ERROR)네트워크";
+                break;
+            case AdInfoKey.AD_SERVER_ERROR:
+                log = "[ " + errorCode + " ] " + "(ERROR)서버";
+                break;
+            case AdInfoKey.AD_API_TYPE_ERROR:
+                log = "[ " + errorCode + " ] " + "(ERROR)API 형식 오류";
+                break;
+            case AdInfoKey.AD_APP_ID_ERROR:
+                log = "[ " + errorCode + " ] " + "(ERROR)ID 오류";
+                break;
+            case AdInfoKey.AD_WINDOW_ID_ERROR:
+                log = "[ " + errorCode + " ] " + "(ERROR)ID 오류";
+                break;
+            case AdInfoKey.AD_ID_BAD:
+                log = "[ " + errorCode + " ] " + "(ERROR)ID 오류";
+                break;
+            case AdInfoKey.AD_CREATIVE_ERROR:
+                log = "[ " + errorCode + " ] " + "(ERROR)광고 생성 불가";
+                break;
+            case AdInfoKey.AD_ETC_ERROR:
+                log = "[ " + errorCode + " ] " + "(ERROR)예외 오류";
+                break;
+            case AdInfoKey.CREATIVE_FILE_ERROR:
+                log = "[ " + errorCode + " ] " + "(ERROR)파일 형식";
+                break;
+            case AdInfoKey.AD_INTERVAL:
+                log = "[ " + errorCode + " ] " + "광고 요청 어뷰징";
+                break;
+            case AdInfoKey.AD_TIMEOUT:
+                log = "[ " + errorCode + " ] " + "광고 API TIME OUT";
+                break;
+            case AdInfoKey.AD_ADCLICK:
+                log = "[ " + errorCode + " ] " + "광고 클릭";
+                break;
+            default:
+                log = "[ " + errorCode + " ] " + "etc";
+                break;
+        }
+        Log.e(getClass().getName(), log);
     }
 }
